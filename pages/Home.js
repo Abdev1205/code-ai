2a63e46f3eba8743dc337897cc752fe3cf9541e7
Abhay1205Mishra <97285855+Abhay1205Mishra@users.noreply.github.com>
2023-07-30 11:27:21 +0530

    basic achnges

--- pages/Home.js
+++ pages/Home.js
@@ -1,78 +1,78 @@
-import { useState } from "react"
+// pages/index.js (Home.js)
+
+import React, { useState } from "react";
 
 const Home = () => {
-  const [apiOutput, setApiOutput] = useState('')
-  const [isGenerating, setIsGenerating] = useState(false)
-  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [userInput, setUserInput] = useState("");
 
   const onUserChangedText = (e) => {
     setUserInput(e.target.value);
-  }
+  };
 
   const callGenerateEndpoint = async () => {
     setIsGenerating(true);
 
-    console.log("Calling OpenAI...")
-    const response = await fetch('/api/generate', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ userInput }),
-    });
+    console.log("Calling server-side API...");
+    try {
+      const response = await fetch("/api/generate", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userInput }),
+      });
 
-    const data = await response.json();
-    const { output } = data;
-    console.log("OpenAI replied...", output.text)
+      if (!response.ok) {
+        throw new Error("API request failed");
+      }
 
-    setApiOutput(`${output.text}`);
-    setIsGenerating(false);
-  }
+      const data = await response.json();
+      const { output } = data;
+      console.log("API response received...", output.text);
+
+      setApiOutput(`${output.text}`);
+    } catch (error) {
+      console.error("Error while calling the API:", error);
+      setApiOutput("Error while generating text. Please try again later.");
+    } finally {
+      setIsGenerating(false);
+    }
+  };
 
   return (
-    <>
-      <div className="prompt-container">
+    <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md mx-auto bg-white shadow-md rounded-lg p-8">
         <textarea
-          placeholder="start typing here"
-          className="prompt-box"
+          placeholder="Start typing here"
+          className="prompt-box p-4 border border-gray-300 rounded-md w-full h-40 focus:outline-none focus:ring focus:border-blue-300"
           value={userInput}
           onChange={(e) => onUserChangedText(e)}
         />
-        <div className="prompt-buttons">
-          <a className="generate-button" onClick={callGenerateEndpoint}>
-            <div className="generate">
-              <p>Generate</p>
-            </div>
-          </a>
+        <div className="mt-4">
+          <button
+            className={`generate-button ${isGenerating ? "loading" : ""
+              } px-4 py-2 bg-blue-500 text-white rounded-md focus:outline-none`}
+            onClick={callGenerateEndpoint}
+            disabled={isGenerating}
+          >
+            {isGenerating ? (
+              <span className="loader"></span>
+            ) : (
+              <span>Generate</span>
+            )}
+          </button>
         </div>
-        {/* New code I added here */}
         {apiOutput && (
-          <div className="output">
-            <div className="output-header-container">
-              <div className="output-header">
-                <h3>Output</h3>
-              </div>
-            </div>
-            <div className="output-content">
-              <p>{apiOutput}</p>
-            </div>
+          <div className="output mt-4 bg-gray-50 p-4 rounded-md">
+            <h3 className="font-bold mb-2">Output</h3>
+            <p>{apiOutput}</p>
           </div>
         )}
       </div>
+    </div>
+  );
+};
 
-      <div className="prompt-buttons">
-        <a
-          className={isGenerating ? 'generate-button loading' : 'generate-button'}
-          onClick={callGenerateEndpoint}
-        >
-          <div className="generate">
-            {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
-          </div>
-        </a>
-      </div>
-
-    </>
-  )
-}
-
-export default Home
+export default Home;
--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,4 +1,9 @@
+// pages/api/generate.js
+
 import { Configuration, OpenAIApi } from 'openai';
+
+const MIN_API_CALL_INTERVAL = 1000; // Minimum time (in milliseconds) between API calls
+let lastApiCallTime = 0;
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -7,20 +12,34 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const basePromptPrefix = "";
-const generateAction = async (req, res) => {
-  // Run first prompt
-  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
-  const baseCompletion = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
-    temperature: 0.7,
-    max_tokens: 250,
-  });
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).end(); // Method Not Allowed
+  }
 
-  const basePromptOutput = baseCompletion.data.choices.pop();
+  // Check if the time since the last API call is greater than the minimum interval
+  const currentTime = Date.now();
+  if (currentTime - lastApiCallTime < MIN_API_CALL_INTERVAL) {
+    return res.status(429).json({ error: "Too many requests. Please wait a moment and try again." });
+  }
 
-  res.status(200).json({ output: basePromptOutput });
-};
+  lastApiCallTime = currentTime;
 
-export default generateAction;
+  try {
+    const userInput = req.body.userInput;
+
+    const baseCompletion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${basePromptPrefix}${userInput}`,
+      temperature: 0.7,
+      max_tokens: 250,
+    });
+
+    const basePromptOutput = baseCompletion.data.choices.pop();
+    res.status(200).json({ output: basePromptOutput });
+  } catch (error) {
+    console.error('Error while calling the API:', error);
+    res.status(500).json({ error: 'Something went wrong with the API call.' });
+  }
+}
